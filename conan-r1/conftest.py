b771c98b502a4abb425aac231d9fd3fff9b496f6
byf <yongfengbu@chd.edu.cn>
2026-03-29 15:35:11 +0800

    Delete conan-r1 directory

--- conan-r1/conftest.py
+++
@@ -1,5 +0,0 @@
-"""pytest configuration: add project root to sys.path."""
-import sys
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).parent))
--- conan-r1/evaluation/__init__.py
+++
@@ -1,1 +0,0 @@
-# evaluation package
